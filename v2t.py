d6ea958aaa3b78819b245497d3493ec3ef351007
Maria <60796940+MariaKudryashova@users.noreply.github.com>
2023-11-19 22:05:11 +0600

    update article

--- v2t.py
+++ v2t.py
@@ -1,10 +1,9 @@
-from moviepy.editor import VideoFileClip
+from openai import OpenAI
 from pydub import AudioSegment
+from moviepy.editor import VideoFileClip
 import speech_recognition as sr
-import openai
 
-import requests
-from urllib3 import disable_warnings
+client = OpenAI()
 
 def convert_mp4_to_wav(input_file, output_file):
     # Загружаем видеофайл
@@ -16,76 +15,121 @@ def convert_mp4_to_wav(input_file, output_file):
     # Сохраняем аудио в формате wav
     audio_clip.write_audiofile(output_file, codec='pcm_s16le') #ogg:'libvorbis'
 
+# Функция для распознавания речи в аудиофайле с использованием библиотеки SpeechRecognition
 def recognize_audio(audio_file_path, chunk_duration_ms=10000):
     # Загрузка аудио файла
     audio = AudioSegment.from_wav(audio_file_path)
 
-    # Получение длительности аудио в миллисекундах
-    audio_duration_ms = len(audio)
-
-    # Разбивка на части и распознавание речи
+    # Инициализация объекта Recognizer из библиотеки SpeechRecognition
     recognizer = sr.Recognizer()
+
+    # Пустая строка для хранения распознанного текста
     text = ""
 
-    # Расчет общего количества итераций
-    total_iterations = audio_duration_ms // chunk_duration_ms + 1
+    # Определение размера куска аудио в миллисекундах
+    chunk_size = int(chunk_duration_ms)
 
-    for i in range(0, audio_duration_ms, chunk_duration_ms):
-        
-        print(f"Итерация {i // chunk_duration_ms + 1} из {total_iterations}")
-        
-        # Выборка части аудио
-        chunk = audio[i:i + chunk_duration_ms]
+    # Разделение аудио на куски заданного размера
+    chunks = [audio[i:i + chunk_size] for i in range(0, len(audio), chunk_size)]
+    
+    # Итерация по каждому куску аудио
+    for i, chunk in enumerate(chunks):
+        # Создание временного файла для текущего куска
+        chunk_path = f"chunk_{i}.wav"
+        chunk.export(chunk_path, format="wav")
 
-        # Сохранение временного файла
-        chunk.export("temp_chunk.wav", format="wav")
+        # Использование AudioFile из SpeechRecognition для открытия временного файла
+        with sr.AudioFile(chunk_path) as source:
+            # Запись аудио из файла
+            chunk_audio = recognizer.record(source)
 
-        # Распознавание речи        
-        with sr.AudioFile("temp_chunk.wav") as source:
-            audio_chunk = recognizer.record(source)
             try:
-                # Попытка распознавания речи
-                chunk_text = recognizer.recognize_google(audio_chunk, language="ru-RU")
-                text += chunk_text + " "
+                # Попытка распознавания речи с использованием Google Speech Recognition
+                chunk_text = recognizer.recognize_google(chunk_audio, language="ru-RU")
                 
-            except sr.UnknownValueError:
-                print("Google Speech Recognition не смог распознать аудио")
+                # Добавление распознанного текста к общему тексту
+                text += chunk_text + " "
 
+                # Вывод распознанного текста для текущего куска
+                print(chunk_text)
+                print("")
+            except sr.UnknownValueError:
+                # Обработка случая, когда Google Speech Recognition не может распознать аудио
+                print(f"Google Speech Recognition не смог распознать аудио для куска {i}")
+
+    # Возврат общего распознанного текста
     return text
 
+
 def save_to_text_file(result_text, output_file_path):
-    with open(output_file_path, "w", encoding="utf-8") as file:
+    with open(output_file_path, "a", encoding="utf-8") as file:
         file.write(result_text)
 
-def summary_text(file_path):
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
+def split_text(text, max_chunk_size = 2048):
     
-    # Запрос к OpenAI API для выделения главной мысли
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=text,
-        max_tokens=50  # Выберите количество токенов в зависимости от вашего текста
-    )
+    chunks = []
+    current_chunk = ""
+    for sentence in text.split("."):
+        if len(current_chunk) + len(sentence) < max_chunk_size:
+            current_chunk += sentence + "."
+        else:
+            chunks.append(current_chunk.strip())
+            current_chunk = sentence + "."
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    return chunks
 
-    # Извлечение ответа сгенерированного моделью
-    main_idea = response.choices[0].text.strip()
 
-    # Вывод результата
-    print("Главная мысль: ", main_idea)
+# Функция для генерации краткого содержания текста с использованием OpenAI GPT-3
+def generate_summary(text, max_chunk_size=2048):
+    # Разбивка входного текста на куски с заданным максимальным размером
+    input_chunks = split_text(text, max_chunk_size)
+    print("Text split ")
+    print(len(input_chunks))
+    
+    # Список для хранения сгенерированных кусков текста
+    output_chunks = []
+    
+    # Задание промпта для модели
+    #prompt = "напиши важные тезисы из текста "
+    prompt = " выдели только самые важные тезисы очень кратко "
 
-disable_warnings()
+    # Итерация по каждому куску входного текста
+    for i, chunk in enumerate(input_chunks):
+        # Запрос к модели GPT-3 для генерации тезисов на основе текущего куска текста
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": prompt + chunk}
+            ],
+            temperature=0.1
+        )
+        
+        # Извлечение сгенерированного ответа от модели
+        answer = response.choices[0].message.content
+        
+        # Вывод информации о текущем куске
+        print(f"chunk {i+1} from {len(input_chunks)}")
+        print(answer)
+        
+        # Добавление сгенерированного куска в список
+        output_chunks.append(answer)
+    
+    # Объединение сгенерированных кусков в одну строку и возврат результата
+    return " ".join(output_chunks)
 
-openai.api_host = 'https://api.openai.com'
-openai.api_base = openai.api_host
-openai.verify_ssl_certs = False
-
-with open("apikey.txt", 'r') as file:
-    openai.api_key = file.read().strip()
 
 #convert_mp4_to_wav('input.mp4', 'output.wav')
 #text_result = recognize_audio('output.wav')
 #save_to_text_file(text_result, "result.txt")
 
-summary_text("result.txt")
+with open("result.txt", 'r', encoding='utf-8') as file:
+    text = file.read()
+
+summary = generate_summary(text)
+print("Summary")
+print(summary)
+
+with open("summary.txt", "w", encoding="utf-8") as file:
+    file.write(summary)
+
